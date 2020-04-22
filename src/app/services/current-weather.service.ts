65fc69021d14dd7904687f082111165a2f17a66f
lfmll <luisfernandomedinallorenti@gmail.com>
2020-04-22 00:03:04 -0400

    Obtener clima actual de OpenWeather

--- src/app/services/current-weather.service.ts
+++ src/app/services/current-weather.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Subject, Observable} from 'rxjs';
 
+import {environment} from '../../environments/environment';
+import {Coords} from '../../structures/coords.structure';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,11 +11,16 @@ export class CurrentWeatherService {
   public weatherSubject: Subject<any>=new Subject<any>();
   public weather$:Observable<any>=this.weatherSubject.asObservable();
 
+  endpoint:string='https://api.openweathermap.org/data/2.5/weather';
   constructor(private http:HttpClient) {
-    this.get();
+    this.get({
+      lat:35,
+      lon:139
+    });
   }
-  get(){
-    let Observable=this.http.get('assets/weather.json').subscribe(this.weatherSubject);
+  get(coords:Coords){
+    let args:string=`?lat=${coords.lat}&lon=${coords.lon}&APPID=${environment.key}&units=metric`;
+    let Observable=this.http.get(this.endpoint+args).subscribe(this.weatherSubject);
     //this.http.get('assets/weather.json').subscribe(console.log);
   }
   //Subject
