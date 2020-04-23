9366291028b523d7bf979d0500022d5ee07741b1
lfmll <luisfernandomedinallorenti@gmail.com>
2020-04-22 20:39:12 -0400

    Iconos animados para el clima

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { WeatherIconComponent } from './weather-icon/weather-icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    WeatherIconComponent
   ],
   imports: [
     BrowserModule,
