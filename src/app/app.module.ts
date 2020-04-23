6edf96efa3fbbb9049e4e479d038d92df74629b1
lfmll <luisfernandomedinallorenti@gmail.com>
2020-04-22 23:54:34 -0400

    mostrar pronostico del clima

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherIconComponent } from './weather-icon/weather-icon.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CurrentWeatherComponent,
     WeatherIconComponent,
-    WeatherCardComponent
+    WeatherCardComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
