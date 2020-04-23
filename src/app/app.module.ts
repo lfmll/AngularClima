ebde0b43260cb3b331881bb6d7dbdd993526f770
lfmll <luisfernandomedinallorenti@gmail.com>
2020-04-23 18:48:36 -0400

    animaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherIconComponent } from './weather-icon/weather-icon.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { ForecastComponent } from './forecast/forecast.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { ForecastComponent } from './forecast/forecast.component';
     CurrentWeatherComponent,
     WeatherIconComponent,
     WeatherCardComponent,
-    ForecastComponent
+    ForecastComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {ForecastService} from '../services/forecast.service';
+import {showUpStaggered} from '../animations/showUp.animation';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.sass']
+  styleUrls: ['./forecast.component.sass'],
+  animations:[showUpStaggered]
 })
 export class ForecastComponent implements OnInit {
 
